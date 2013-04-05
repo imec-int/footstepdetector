afcf95fe966581f7e620dfc29ce7ef7142132ed8
Sam Decrock <sam.decrock@gmail.com>
2013-04-05 17:47:29 +0200

    Beter dan niets voor een 20% vrijdag :-)

---
+++ app/public/javascripts/index.js
@@ -0,0 +1,95 @@
+var cv, ctx;
+
+function pageLoaded(){
+
+	cv = document.getElementById("cv");
+	ctx = cv.getContext("2d");
+	ctx.fillStyle="#000000";
+	ctx.fillRect(0,0,cv.width,cv.height);
+
+
+	var audioContext = new webkitAudioContext();
+
+	navigator.webkitGetUserMedia({audio:true}, function (stream){
+
+		var sourceNode = audioContext.createMediaStreamSource(stream);
+		var finalDestinationNode = audioContext.destination;
+		var gainNode = audioContext.createGainNode();
+		var scriptProcessorNode = audioContext.createScriptProcessor(2048,1,1);
+
+		gainNode.gain.value = 10;
+
+		scriptProcessorNode.onaudioprocess = function (event){
+
+			var inputChannel0 = event.inputBuffer.getChannelData(0);
+			var outputChannel0 = event.outputBuffer.getChannelData(0);
+
+			for(var i in inputChannel0){
+				//outputChannel0[i] = inputChannel0[i];
+
+				drawPoint( inputChannel0[i] );
+			}
+
+		}
+
+		sourceNode.connect( gainNode );
+		gainNode.connect( scriptProcessorNode );
+		scriptProcessorNode.connect( finalDestinationNode ); // m'n source connecteren met de output;
+
+		$("#gain").val( gainNode.gain.value );
+
+		// gain change handler:
+		$("#gain").change(function (event){
+			gainNode.gain.value = parseInt( $("#gain").val() );
+		});
+
+		// debug brol:
+		setTimeout(function(){
+			console.log("audioContext.sampleRate: " + audioContext.sampleRate);
+			console.log("audioContext.currentTime: " + audioContext.currentTime);
+
+			console.log("sourceNode.numberOfInputs: " + sourceNode.numberOfInputs);
+			console.log("sourceNode.numberOfOutputs: " + sourceNode.numberOfOutputs);
+			console.log("finalDestinationNode.numberOfInputs: " + finalDestinationNode.numberOfInputs);
+			console.log("finalDestinationNode.numberOfOutputs: " + finalDestinationNode.numberOfOutputs);
+			console.log("finalDestinationNode.maxChannelCount: " + finalDestinationNode.maxChannelCount);
+
+		}, 1000);
+
+
+	}, function () {
+		alert('Stream generation failed.');
+	});
+
+}
+
+function draw(timeData){
+	var timeLength = timeData.length;
+	var c = ctx;
+
+	//bg
+	c.fillStyle="#EEEEEE";
+	c.fillRect(0,0,cv.width,cv.height);
+
+	// scale time data
+	//timeData = 128 + (timeData-128)*4;
+
+	for (var i = 0; i < timeLength; i++) {
+		c.fillStyle="#FF0000";
+		c.fillRect(i, 256-timeData[i],1,1);
+	};
+}
+
+
+var x = 0;
+function drawPoint(value){
+	ctx.fillStyle="#FF0000";
+	ctx.fillRect(x, cv.height/2-(value*100), 1, 1);
+	x = x + 0.001;
+}
+
+
+$(pageLoaded); //jQuery onload
+
+
+
