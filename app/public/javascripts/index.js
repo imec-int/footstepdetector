ccdb0d660563d2bb65af9dbf4f62156ecb27a08b
Sam Decrock <sam.decrock@gmail.com>
2013-04-08 10:59:42 +0200

    it kinda works (telt stappen)

--- app/public/javascripts/index.js
+++ app/public/javascripts/index.js
@@ -1,11 +1,21 @@
 var cv, ctx;
 
+var footstepThreshold = 5.2;
+var minFootstepDuration = 1;
+
+var stepCounter = 0;
+var maxvolume = -9999;
+var minvolume = 9999;
+
 function pageLoaded(){
 
 	cv = document.getElementById("cv");
 	ctx = cv.getContext("2d");
 	ctx.fillStyle="#000000";
 	ctx.fillRect(0,0,cv.width,cv.height);
+
+
+
 
 
 	var audioContext = new webkitAudioContext();
@@ -15,19 +25,55 @@ function pageLoaded(){
 		var sourceNode = audioContext.createMediaStreamSource(stream);
 		var finalDestinationNode = audioContext.destination;
 		var gainNode = audioContext.createGainNode();
-		var scriptProcessorNode = audioContext.createScriptProcessor(2048,1,1);
+		var scriptProcessorNode = audioContext.createScriptProcessor(1024,1,1);
 
 		gainNode.gain.value = 10;
 
+		var prevVolumeValue = 0;
+
+		var footstepDuration = 0;
+		var footstepInProgress = false;
 		scriptProcessorNode.onaudioprocess = function (event){
 
-			var inputChannel0 = event.inputBuffer.getChannelData(0);
-			var outputChannel0 = event.outputBuffer.getChannelData(0);
+			var inputChannel = event.inputBuffer.getChannelData(0);
 
-			for(var i in inputChannel0){
-				//outputChannel0[i] = inputChannel0[i];
 
-				drawPoint( inputChannel0[i] );
+			for(var i in inputChannel){
+				if( !isNaN(inputChannel[i]) )
+					drawPoint( inputChannel[i] );
+			}
+
+
+			// gemiddelde (volume) berekenen:
+			var volume = 0;
+			for(var i in inputChannel){
+				if( !isNaN(inputChannel[i]) ){
+					volume += Math.abs( inputChannel[i] );
+				}
+			}
+			volume = volume / 1024;
+
+			// tekenen:
+			drawLine(prevVolumeValue, volume);
+			prevVolumeValue = volume;
+
+			// debugwaarden bijhouden:
+			if(volume > maxvolume)
+				maxvolume = volume;
+
+			if(volume < minvolume)
+				minvolume = volume;
+
+			// voetstap detecteren:
+			if(volume > footstepThreshold){
+				footstepDuration++;
+
+				if(footstepDuration > minFootstepDuration){
+					stepCounter++;
+				}
+			}else{
+				// terug resetten:
+				footstepDuration = 0;
 			}
 
 		}
@@ -63,11 +109,45 @@ function pageLoaded(){
 
 }
 
+
+
 var x = 0;
+
 function drawPoint(value){
 	ctx.fillStyle="#FF0000";
-	ctx.fillRect(x, cv.height/2-(value*100), 1, 1);
+	ctx.fillRect(x, cv.height/4-(value*100), 1, 1);
 	x = x + 0.001;
+}
+
+function drawLine(from, to){
+	ctx.beginPath();
+	ctx.moveTo(x, cv.height-(from*500-2400));
+	ctx.lineTo(x, cv.height-(to*500-2400));
+	ctx.lineWidth = 1;
+	ctx.strokeStyle = '#00FF00';
+	ctx.stroke();
+
+	currentValue = (to*500-2400);
+}
+
+
+
+
+
+var currentValue = 0;
+
+updateDebugData();
+
+function updateDebugData(){
+	$("#min").html( minvolume );
+	$("#max").html( maxvolume );
+
+	$("#value").html( currentValue );
+
+	$("#steps").html( stepCounter );
+
+
+	window.webkitRequestAnimationFrame( updateDebugData );
 }
 
 $(pageLoaded); //jQuery onload
